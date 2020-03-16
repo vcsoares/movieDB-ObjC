094531ad60efa7c5dbea7f0f9cd027ad0dd0fc0e
Vinícius Chagas <vcsoares@inf.ufrgs.br>
2020-03-16 17:35:45 -0300

    Declare Movie model properties

---
+++ movieDB-objc/Models/Movie.h
@@ -0,0 +1,25 @@
+//
+//  Movie.h
+//  movieDB-objc
+//
+//  Created by Vinícius Chagas on 16/03/20.
+//  Copyright © 2020 Vinícius Chagas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Movie : NSObject
+
+@property NSNumber* uid;
+
+@property NSString* name;
+@property NSString* synopsis;
+@property NSString* genres;
+@property NSNumber* rating;
+@property NSURL* picture;
+
+@end
+
+NS_ASSUME_NONNULL_END
