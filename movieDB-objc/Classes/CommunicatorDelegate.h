460e66d32e5d275bcc5b2e7315807004a81ee912
Vinícius Chagas <vcsoares@inf.ufrgs.br>
2020-03-17 16:52:36 -0300

    Define CommunicatorDelegate protocol

---
+++ movieDB-objc/Classes/CommunicatorDelegate.h
@@ -0,0 +1,20 @@
+//
+//  CommunicatorDelegate.h
+//  movieDB-objc
+//
+//  Created by Vinícius Chagas on 17/03/20.
+//  Copyright © 2020 Vinícius Chagas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol CommunicatorDelegate <NSObject>
+
+-(void)receivedMovieList:(NSData*) json;
+-(void)fetchMovieListFailedWithError:(NSError*) error;
+
+@end
+
+NS_ASSUME_NONNULL_END
