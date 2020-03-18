6f1d1c159647f2062135b965d6ad866f5e4ab3f1
Vinícius Chagas <vcsoares@inf.ufrgs.br>
2020-03-18 16:51:51 -0300

    Add Genre model

---
+++ movieDB-objc/Models/Genre.h
@@ -0,0 +1,20 @@
+//
+//  Genre.h
+//  movieDB-objc
+//
+//  Created by Vinícius Chagas on 18/03/20.
+//  Copyright © 2020 Vinícius Chagas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Genre : NSObject
+
+@property NSInteger* id;
+@property NSString* name;
+
+@end
+
+NS_ASSUME_NONNULL_END
