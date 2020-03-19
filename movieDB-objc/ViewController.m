ebb2362fe2b782a449b34bbe4115e1b429bd5a82
Vinícius Chagas <vcsoares@inf.ufrgs.br>
2020-03-19 17:18:18 -0300

    Show star symbol for movie ratings

--- movieDB-objc/ViewController.m
+++ movieDB-objc/ViewController.m
@@ -101,7 +101,14 @@
     if (movie != nil) {
         [cell.movieTitleLabel setText:movie.title];
         [cell.movieOverviewLabel setText:movie.overview];
-        [cell.movieRatingLabel setText:movie.vote_average.stringValue];
+        
+        NSMutableAttributedString* rating = [[NSMutableAttributedString alloc] initWithString:movie.vote_average.stringValue];
+        
+        NSTextAttachment* rating_symbol = [NSTextAttachment textAttachmentWithImage:[UIImage systemImageNamed:@"star"]];
+        NSAttributedString* rating_symbol_str = [NSAttributedString attributedStringWithAttachment:rating_symbol];
+        [rating replaceCharactersInRange:NSMakeRange(0, 0) withAttributedString:rating_symbol_str];
+        
+        [cell.movieRatingLabel setAttributedText:rating];
         
         NSURLSessionDownloadTask* poster_download = [[NSURLSession sharedSession] downloadTaskWithURL:movie.poster_path completionHandler:^(NSURL * _Nullable location, NSURLResponse * _Nullable response, NSError * _Nullable error) {
             UIImage* poster = [UIImage imageWithData:[NSData dataWithContentsOfURL:location]];
