5244b99b5b9feaaa9b6c74e90911e16abb29ca50
Vinícius Chagas <vcsoares@inf.ufrgs.br>
2020-03-19 15:50:16 -0300

    Display textual information from loaded movies in their cells

--- movieDB-objc/ViewController.m
+++ movieDB-objc/ViewController.m
@@ -11,12 +11,15 @@
 #import "CommunicatorDelegate.h"
 #import "Parser.h"
 #import "Movie.h"
+#import "MovieTableViewCell.h"
 
-@interface ViewController () <CommunicatorDelegate>
+@interface ViewController () <CommunicatorDelegate, UITableViewDelegate, UITableViewDataSource>
 
 @property Communicator* communicator;
 @property NSArray* popularMovieList;
 @property NSArray* nowPlayingMovieList;
+
+@property (weak, nonatomic) IBOutlet UITableView *moviesTableView;
 
 @end
 
@@ -24,7 +27,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.moviesTableView.delegate = self;
+    self.moviesTableView.dataSource = self;
+    
     self.communicator = [[Communicator alloc] init];
     self.communicator.delegate = self;
     [self.communicator fetchMovieList:FetchPopular];
@@ -62,12 +68,75 @@
             [self.communicator fetchMovieDetails:movie];
             break;
     }
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self.moviesTableView reloadData];
+    });
 }
 
 - (void)receivedMovieDetails:(nonnull NSData *)json for:(nonnull Movie *)movie {
     NSError* error = nil;
     [Parser detailsForMovie:movie from:json error:&error];
     NSLog(@"%@",movie.description);
+}
+
+#pragma mark - Table View Data Source -
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    MovieTableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"MovieCell"];
+    
+    if (cell == nil) {
+        return [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"default"];
+    }
+     
+    Movie* movie;
+    
+    switch (indexPath.section) {
+        case 0:
+            movie = self.popularMovieList[indexPath.row];
+            break;
+            
+        default:
+            movie = self.nowPlayingMovieList[indexPath.row];
+            break;
+    }
+    
+    [cell.movieTitleLabel setText:movie.title];
+    [cell.movieOverviewLabel setText:movie.overview];
+    [cell.movieRatingLabel setText:movie.vote_average.stringValue];
+    
+    return cell;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 2;
+}
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    switch (section) {
+        case 0:
+            NSLog(@"-!!!- SECTION: POPULAR --- COUNT: %d", (int)self.popularMovieList.count);
+            return self.popularMovieList.count;
+            break;
+            
+        default:
+            NSLog(@"-!!!- SECTION: NOW PLAYING --- COUNT: %d", (int)self.nowPlayingMovieList.count);
+            return self.nowPlayingMovieList.count;
+            break;
+    }
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    switch (section) {
+        case 0:
+            NSLog(@"-!!!- SECTION: POPULAR");
+            return @"Popular movies";
+            break;
+            
+        default:
+            NSLog(@"-!!!- SECTION: NOW PLAYING");
+            return @"Now playing";
+            break;
+    }
 }
 
 @end
