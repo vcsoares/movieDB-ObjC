ee9daa9cdab9d37a8cd11f055ddf4ecb926a9ff2
Vinícius Chagas <vcsoares@inf.ufrgs.br>
2020-03-23 16:23:37 -0300

    Add target for Swift client

---
+++ moviedb-swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  moviedb-swift
+//
+//  Created by Vinícius Chagas on 23/03/20.
+//  Copyright © 2020 Vinícius Chagas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ moviedb-swift/Models/Movie.swift
@@ -0,0 +1,9 @@
+//
+//  Movie.swift
+//  moviedb-swift
+//
+//  Created by Vinícius Chagas on 23/03/20.
+//  Copyright © 2020 Vinícius Chagas. All rights reserved.
+//
+
+import Foundation
