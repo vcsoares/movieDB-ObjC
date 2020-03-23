34e60c4686b61ce21b4d7b5a2c3b44ac7db2ade2
Vinícius Chagas <vcsoares@inf.ufrgs.br>
2020-03-23 17:15:28 -0300

    Fetch movies via Obj-C communicator

--- moviedb-swift/ContentView.swift
+++ moviedb-swift/ContentView.swift
@@ -8,9 +8,45 @@
 
 import SwiftUI
 
+class MovieStorage : NSObject, ObservableObject, CommunicatorDelegate {
+    func receivedMovieList(_ json: Data, from option: FetchOption) {
+        let error = NSErrorPointer(nilLiteral: ())
+        let movies = Parser.movieList(fromJSON: json, error: error)
+        if error?.pointee != nil {
+            print("-X-X- MOVIE LIST ERROR")
+            return
+        }
+        if !movies.isEmpty {
+            DispatchQueue.main.async { [weak self] in
+                self?.nowPlaying = movies as! [Movie]
+            }
+        }
+    }
+    
+    func receivedMovieDetails(_ json: Data, for movie: Movie) {
+        print("a")
+    }
+    
+    func fetchFailedWithError(_ error: Error) {
+        print("-XXX- FETCH FAILED")
+        print(error.localizedDescription)
+    }
+    
+    @Published var nowPlaying : [Movie] = []
+    @Published var popular : [Movie] = []
+}
+
 struct ContentView: View {
+    @ObservedObject var storage = MovieStorage()
+    
     var body: some View {
         Text("Hello, World!")
+    }
+    
+    init() {
+        var c = Communicator()
+        c.delegate = storage
+        c.fetchMovieList(.nowPlaying)
     }
 }
 
